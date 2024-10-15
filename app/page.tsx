f93d893242c50647d79922ea052058d1f1a76508
felixvnolasco84 <felix@polygonag.com>
2024-10-15 10:09:44 -0600

    removed padding top

--- app/page.tsx
+++ app/page.tsx
@@ -105,7 +105,7 @@ const projects = [
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col gap-10 py-24 lg:gap-20">
+    <main className="relative flex flex-col gap-10 pb-24 lg:gap-20">
       <HeroCarrousel />
       <AboutSection />
 
